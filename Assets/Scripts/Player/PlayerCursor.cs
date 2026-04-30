71e30dca4e5c0dedc289b360794286817d416216
Darcy Mazloum <damazinator11@gmail.com>
2026-04-30 19:11:19 -0400

    Working on mouse cursor

--- Assets/Scripts/Player/PlayerCursor.cs
+++ Assets/Scripts/Player/PlayerCursor.cs
@@ -8,9 +8,23 @@ public class PlayerCursor : MonoBehaviour
     private GeneralActions inputActions;
     private InputAction select;
 
+    [SerializeField] private Transform CameraTransform;
+    [SerializeField] private LayerMask SelectableLayer;
+    [SerializeField] private float MaxSelectDistance;
+
+    private Vector3 MousePos;
+
     private void Awake()
     {
         inputActions = new GeneralActions();
+    }
+
+    private void Update()
+    {
+        // This is apparently the mouse pos in pixels and not world coords...
+        // Gotta translate that
+        MousePos = Mouse.current.position.ReadValue();
+        Debug.Log("Update (MousePos): " + MousePos);
     }
 
     private void OnEnable()
@@ -29,7 +43,16 @@ public class PlayerCursor : MonoBehaviour
     {
         if (context.performed)
         {
-            Debug.Log("Select pressed");
+            
+
+            Debug.Log("Select pressed: " + MousePos);
+
+            // Make a vector that starts from the camera position and ends at the cursor... i think... gotta figure that out one sec
+
+            //RaycastHit hit;
+            //bool didHit = Physics.Raycast(CameraTransform.position, transform.forward, out hit, MaxSelectDistance, SelectableLayer);
+            // camera's transform, direction vector (calculated based on the cursor ugh), out hit, MaxDistance... check this, selectable layer mask
+
         }
     }
 }
